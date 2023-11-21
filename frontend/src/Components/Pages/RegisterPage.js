00825d080808323086b4083838370d59f723c652
loic-bouffioux-vinci <loic.bouffioux@student.vinci.be>
2023-11-21 09:49:34 +0100

    updqte frontend register

--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -6,7 +6,7 @@ const ConnectionPage = () => {
 
   // Création de la structure de la page de connexion
   const connectionPageContent = document.createElement('div');
-  connectionPageContent.classList.add('connection-page-content');
+  connectionPageContent.classList.add('register-page-content');
   connectionPageContent.style.backgroundImage = `url(${ecranRanking})`; // Ajout de l'image comme fond d'écran
   connectionPageContent.style.backgroundSize = 'cover'; // Ajustez la taille de l'image selon vos besoins
   connectionPageContent.style.backgroundPosition = 'center'; // Ajustez la position de l'image selon vos besoins
@@ -22,7 +22,7 @@ const ConnectionPage = () => {
   form.classList.add('bg-light', 'p-4', 'rounded');
 
   const usernameLabel = document.createElement('label');
-  usernameLabel.textContent = 'Nom d\'utilisateur';
+  usernameLabel.textContent = 'username';
   usernameLabel.classList.add('form-label');
   const usernameInput = document.createElement('input');
   usernameInput.type = 'text';
@@ -31,18 +31,8 @@ const ConnectionPage = () => {
   usernameInput.name = 'username';
   usernameInput.required = true;
 
-  const emailLabel = document.createElement('label');
-  emailLabel.textContent = 'Email';
-  emailLabel.classList.add('form-label');
-  const emailInput = document.createElement('input');
-  emailInput.type = 'email';
-  emailInput.classList.add('form-control');
-  emailInput.id = 'email';
-  emailInput.name = 'email';
-  emailInput.required = true;
-
   const passwordLabel = document.createElement('label');
-  passwordLabel.textContent = 'Mot de passe';
+  passwordLabel.textContent = 'password';
   passwordLabel.classList.add('form-label');
   const passwordInput = document.createElement('input');
   passwordInput.type = 'password';
@@ -52,7 +42,7 @@ const ConnectionPage = () => {
   passwordInput.required = true;
 
   const confirmPasswordLabel = document.createElement('label');
-  confirmPasswordLabel.textContent = 'Confirmer le mot de passe';
+  confirmPasswordLabel.textContent = 'Confirmation password';
   confirmPasswordLabel.classList.add('form-label');
   const confirmPasswordInput = document.createElement('input');
   confirmPasswordInput.type = 'password';
@@ -62,21 +52,19 @@ const ConnectionPage = () => {
   confirmPasswordInput.required = true;
 
   const submitButton = document.createElement('button');
-  submitButton.textContent = 'Se connecter';
+  submitButton.textContent = 'register';
   submitButton.classList.add('btn', 'btn-primary', 'mt-3');
   submitButton.addEventListener('click', (event) => {
     event.preventDefault(); // Empêche la soumission du formulaire pour l'exemple
     // Ajoutez ici la logique pour soumettre le formulaire de connexion
     const username = usernameInput.value;
-    const email = emailInput.value;
     const password = passwordInput.value;
     const confirmPassword = confirmPasswordInput.value;
 
     // Exemple : Validation basique des champs
-    if (username && email && password && confirmPassword && password === confirmPassword) {
-      alert('Connexion réussie!');
-      window.location.href = '/routes/users';
-      window.location.href = '/';
+    if (username  && password && confirmPassword && password === confirmPassword) {
+      alert('register complet!');
+      <
 
     } else {
       alert('Veuillez remplir tous les champs et assurez-vous que les mots de passe correspondent.');
@@ -86,8 +74,6 @@ const ConnectionPage = () => {
   // Ajout des éléments au formulaire
   form.appendChild(usernameLabel);
   form.appendChild(usernameInput);
-  form.appendChild(emailLabel);
-  form.appendChild(emailInput);
   form.appendChild(passwordLabel);
   form.appendChild(passwordInput);
   form.appendChild(confirmPasswordLabel);
@@ -107,4 +93,4 @@ const ConnectionPage = () => {
   main.appendChild(connectionPageContent);
 };
 
-export default ConnectionPage;
+export default RegisterPage;
