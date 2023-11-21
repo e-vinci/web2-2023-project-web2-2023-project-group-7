a1f3b82d5e128087e9d2edac375a9f7a3af58051
loic-bouffioux-vinci <loic.bouffioux@student.vinci.be>
2023-11-21 09:08:20 +0100

    fonction users correction path

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const {
+  login,
+  register,
+  readOneUserFromUsername,
+  createOneUser,
+  getNextId,
+} = require('../models/users');
+const { authorize, isAdmin } = require('../utils/auths');
 
 const router = express.Router();
 
