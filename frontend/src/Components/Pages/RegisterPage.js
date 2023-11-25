69f7df352eab3b55dd327d8a6e57bf6866f68a04
emeric <emeric.ioos@student.vinci.be>
2023-11-25 11:12:28 +0100

    modification pou register

--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -1,19 +1,19 @@
 // eslint-disable-next-line import/no-unresolved
-import ecranRanking from '../../assets/Capture1.png';
+ import ecranRanking from '../../assets/Capture1.png';
+
 
 const ConnectionPage = () => {
   const main = document.querySelector('main');
 
-  // Création de la structure de la page de connexion
   const connectionPageContent = document.createElement('div');
   connectionPageContent.classList.add('register-page-content');
-  connectionPageContent.style.backgroundImage = `url(${ecranRanking})`; // Ajout de l'image comme fond d'écran
-  connectionPageContent.style.backgroundSize = 'cover'; // Ajustez la taille de l'image selon vos besoins
-  connectionPageContent.style.backgroundPosition = 'center'; // Ajustez la position de l'image selon vos besoins
-  connectionPageContent.style.height = '100vh'; // Utilisation de la hauteur de la fenêtre pour occuper tout l'écran
-  connectionPageContent.style.display = 'flex'; // Utilisation de flexbox pour centrer le contenu
-  connectionPageContent.style.justifyContent = 'center'; // Centrage horizontal
-  connectionPageContent.style.alignItems = 'center'; // Centrage vertical
+  connectionPageContent.style.backgroundImage = `url(${ecranRanking})`;
+  connectionPageContent.style.backgroundSize = 'cover';
+  connectionPageContent.style.backgroundPosition = 'center';
+  connectionPageContent.style.height = '100vh';
+  connectionPageContent.style.display = 'flex';
+  connectionPageContent.style.justifyContent = 'center';
+  connectionPageContent.style.alignItems = 'center';
 
   const formContainer = document.createElement('div');
   formContainer.classList.add('container', 'col-md-4');
@@ -21,96 +21,88 @@ const ConnectionPage = () => {
   const form = document.createElement('form');
   form.classList.add('bg-light', 'p-4', 'rounded');
 
-  const usernameLabel = document.createElement('label');
-  usernameLabel.textContent = 'username';
-  usernameLabel.classList.add('form-label');
-  const usernameInput = document.createElement('input');
-  usernameInput.type = 'text';
-  usernameInput.classList.add('form-control');
-  usernameInput.id = 'username';
-  usernameInput.name = 'username';
-  usernameInput.required = true;
+  const createFormElement = (labelText, inputType, inputName) => {
+    const label = document.createElement('label');
+    label.textContent = labelText;
+    label.classList.add('form-label');
 
-  const emailLabel = document.createElement('label');
-  emailLabel.textContent = 'Email';
-  emailLabel.classList.add('form-label');
-  const emailInput = document.createElement('input');
-  emailInput.type = 'email';
-  emailInput.classList.add('form-control');
-  emailInput.id = 'email';
-  emailInput.name = 'email';
-  emailInput.required = true;
+    const input = document.createElement('input');
+    input.type = inputType;
+    input.classList.add('form-control');
+    input.id = inputName;
+    input.name = inputName;
+    input.required = true;
 
-  const passwordLabel = document.createElement('label');
-  passwordLabel.textContent = 'password';
-  passwordLabel.classList.add('form-label');
-  const passwordInput = document.createElement('input');
-  passwordInput.type = 'password';
-  passwordInput.classList.add('form-control');
-  passwordInput.id = 'password';
-  passwordInput.name = 'password';
-  passwordInput.required = true;
+    return { label, input };
+  };
+  
 
-  const confirmPasswordLabel = document.createElement('label');
-  confirmPasswordLabel.textContent = 'Confirmation password';
-  confirmPasswordLabel.classList.add('form-label');
-  const confirmPasswordInput = document.createElement('input');
-  confirmPasswordInput.type = 'password';
-  confirmPasswordInput.classList.add('form-control');
-  confirmPasswordInput.id = 'confirmPassword';
-  confirmPasswordInput.name = 'confirmPassword';
-  confirmPasswordInput.required = true;
+  const username = createFormElement('Username', 'text', 'username');
+  const password = createFormElement('Password', 'password', 'password');
+  const confirmPassword = createFormElement('Confirm Password', 'password', 'confirmPassword');
 
   const submitButton = document.createElement('button');
-  submitButton.textContent = 'register';
+  submitButton.textContent = 'Register';
   submitButton.classList.add('btn', 'btn-primary', 'mt-3');
-  submitButton.addEventListener('click', (event) => {
-    event.preventDefault(); // Empêche la soumission du formulaire pour l'exemple
-    // Ajoutez ici la logique pour soumettre le formulaire de connexion
-    const username = usernameInput.value;
-    const password = passwordInput.value;
-    const confirmPassword = confirmPasswordInput.value;
-
-    // Exemple : Validation basique des champs
-    if (username  && password && confirmPassword && password === confirmPassword) {
-      alert('register complet!');
-      const response = await fetch(${process.env.API_BASE_URL}/auths/register, {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        mode: 'cors',
-        credentials: 'include',
-        headers: { 'Content-Type': 'application/json' },
-      });
+  submitButton.addEventListener('click', async () => {
     
-      //if (!response.ok) {throw new Error(fetch error : ${response.status} : ${response.statusText});}
+     
+   
+    const formData = {
+       username: username.input.value,
+       password: password.input.value,
+       confirmPassword: confirmPassword.input.value,
+    };
     
-      const authenticatedUser = await response.json();
+    try {
+      
+       // Utilisez votre propre chemin d'API au lieu de '/auths/register'
+       const response = await fetch('/api/register', {
+         method: 'POST',
+         body: JSON.stringify(formData),
+         mode: 'cors',
+         credentials: 'include',
+         headers: { 'Content-Type': 'application/json' }
+         
+       });
+       
+       
+       // console.log("gggggggggggggggggggggggggggggggggggggggggggg")
+      
+        if (!response.ok) {
+          throw new Error(`Fetch error: ${response.status} : ${response.statusText}`);
+        }
+       
+       
+       // const registeredUser = await response.json();
+        // console.log('Registered user:', registeredUser);
+       // console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
+       
+       document.write(response);
 
-    } else {
-      alert('Veuillez remplir tous les champs et assurez-vous que les mots de passe correspondent.');
+       // Ajoutez ici la logique pour rediriger ou effectuer d'autres actions après l'inscription réussie
+    } catch (error) {
+       console.error('Registration failed:', error.message);
+       
     }
+    // window.location.href = '/';
+    
+   });
+
+  [username, password, confirmPassword].forEach(({ label, input }) => {
+    form.appendChild(label);
+    form.appendChild(input);
   });
 
-  // Ajout des éléments au formulaire
-  form.appendChild(usernameLabel);
-  form.appendChild(usernameInput);
-  form.appendChild(passwordLabel);
-  form.appendChild(passwordInput);
-  form.appendChild(confirmPasswordLabel);
-  form.appendChild(confirmPasswordInput);
   form.appendChild(submitButton);
 
-  // Ajout du formulaire au conteneur
   formContainer.appendChild(form);
 
-  // Ajout du conteneur du formulaire à la page de connexion
   connectionPageContent.appendChild(formContainer);
 
-  // Effacement du contenu précédent du main
   main.innerHTML = '';
 
-  // Ajout de la nouvelle structure à la page principale
   main.appendChild(connectionPageContent);
 };
 
-export default RegisterPage;
+export default ConnectionPage;
