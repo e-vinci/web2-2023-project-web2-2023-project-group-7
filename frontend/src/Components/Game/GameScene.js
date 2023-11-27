e7d1408018241667dcb92fb475b719c8e59a3845
devreese <anthony.devreese@student.vinci.be>
2023-11-27 14:00:25 +0100

    new background, more fixes

--- frontend/src/Components/Game/GameScene.js
+++ frontend/src/Components/Game/GameScene.js
@@ -1,20 +1,26 @@
 import Phaser from 'phaser';
 import ScoreLabel from './ScoreLabel';
 
-import skyAsset from '../../assets/sky.png';
+import skyAsset from '../../assets/bosque_tenebre.jpg';
 import platformAsset from '../../assets/platform.png';
 import idleAsset from '../../assets/Samurai/Idle.png';
 import walkAsset from '../../assets/Samurai/Walk.png';
 import jumpAsset from '../../assets/Samurai/Jump.png';
 import healthAsset from '../../assets/health.png';
-import attackAsset from '../../assets/Samurai/Attack_1.png';
+import attack1Asset from '../../assets/Samurai/Attack_1.png';
+import attack2Asset from '../../assets/Samurai/Attack_2.png';
+import attack3Asset from '../../assets/Samurai/Attack_3.png';
 
 const GROUND_KEY = 'ground';
 const IDLE_KEY = 'idle';
 const WALK_KEY = 'walk';
 const JUMP_KEY = 'jump';
-const ATTACK_KEY = 'attack'
-let info;
+const ATTACK1_KEY = 'attack1'
+const ATTACK2_KEY = 'attack2'
+const ATTACK3_KEY = 'attack3'
+let info
+let nbRequired
+let currentAttack = 'attack1'
 
 class GameScene extends Phaser.Scene {
   constructor() {
@@ -24,11 +30,13 @@ class GameScene extends Phaser.Scene {
     this.cursors = undefined;
     this.scoreLabel = undefined;
     this.gameOver = false;
-    this.playerHealth = 10;
+    this.playerHealth = 20;
     this.botHealth = 2;
+    this.playerDamage = 1;
+    this.botDamage = 1;
     this.previousBotHealth = this.botHealth;
     this.previousCollisionTime = 0;
-    this.interval = 3000;
+    this.interval = 1000;
     this.isCollisionDone = false;
   }
 
@@ -49,7 +57,15 @@ class GameScene extends Phaser.Scene {
       frameWidth: 128,
       frameHeight: 128,
     });
-    this.load.spritesheet(ATTACK_KEY, attackAsset, {
+    this.load.spritesheet(ATTACK1_KEY, attack1Asset, {
+      frameWidth: 128,
+      frameHeight: 128,
+    });
+    this.load.spritesheet(ATTACK2_KEY, attack2Asset, {
+      frameWidth: 128,
+      frameHeight: 128,
+    });
+    this.load.spritesheet(ATTACK3_KEY, attack3Asset, {
       frameWidth: 128,
       frameHeight: 128,
     });
@@ -74,6 +90,44 @@ class GameScene extends Phaser.Scene {
     
     /* The Collider takes two objects and tests for collision and performs separation against them.
     Note that we could call a callback in case of collision... */
+
+    // create all animations
+
+    this.anims.create({
+      key: 'walk',
+      frames: this.anims.generateFrameNumbers(WALK_KEY, { start: 0, end: 8}),
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frames: this.anims.generateFrameNumbers(IDLE_KEY, { start: 0, end: 5 }),
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'attack1',
+      frames: this.anims.generateFrameNumbers(ATTACK1_KEY, { start: 0, end: 8}),
+      frameRate: 20,
+      repeat: 0,
+    });
+
+    this.anims.create({
+      key: 'attack2',
+      frames: this.anims.generateFrameNumbers(ATTACK2_KEY, { start: 0, end: 8}),
+      frameRate: 20,
+      repeat: 0,
+    });
+
+    this.anims.create({
+      key: 'attack3',
+      frames: this.anims.generateFrameNumbers(ATTACK3_KEY, { start: 0, end: 8}),
+      frameRate: 20,
+      repeat: 0,
+    });
+
   }
 
   update() {
@@ -89,11 +143,11 @@ class GameScene extends Phaser.Scene {
     
     if (this.cursors.left.isDown) {
       this.player.setVelocityX(-300);
-      this.player.anims.play('left', true);
+      this.player.anims.play('walk', true);
       this.player.flipX = true;
     } else if (this.cursors.right.isDown) {
       this.player.setVelocityX(300);
-      this.player.anims.play('right', true);
+      this.player.anims.play('walk', true);
       this.player.flipX = false;
     } else if(!this.physics.overlap(this.player, this.bot)){
       this.player.setVelocityX(0);
@@ -114,15 +168,15 @@ class GameScene extends Phaser.Scene {
       .setScale(2)
       .refreshBody();
 
-    platforms.create(600, 400, GROUND_KEY);
-    platforms.create(50, 250, GROUND_KEY);
-    platforms.create(750, 220, GROUND_KEY);
+    
     return platforms;
   }
 
   createHealth(){
     const hp = this.add.image(40, 100,'health');
     info = this.add.text(60, 100, this.playerHealth);
+    this.add.text(60,120, 'nb hits required:');
+    nbRequired = this.add.text(225,120, this.botHealth/this.playerDamage);
     return hp;
   }
 
@@ -131,28 +185,21 @@ class GameScene extends Phaser.Scene {
     bot.setBounce(0.2);
     bot.body.setSize(67,74,true).setOffset(20, 54);
 
-    this.anims.create({
-      key: 'avance',
-      frames: this.anims.generateFrameNumbers(WALK_KEY, { start: 0, end: 8}),
-      frameRate: 20,
-      repeat: -1,
-    });
-    
-    this.anims.create({
-      key: 'attack',
-      frames: this.anims.generateFrameNumbers(ATTACK_KEY, { start: 0, end: 8}),
-      frameRate: 5,
-      repeat: 0,
-    });
-
     bot.setVelocityX(-200);
     bot.flipX = true;
+
+    bot.group = this.physics.add.group();
+    bot.group.add(bot);
+
+    this.physics.add.collider(bot.group, this.platforms);
+    
+
     this.time.addEvent({
       delay: 50,
       loop: true,
       callback: () => {
         if(bot.body.velocity.x < 0) {
-          bot.anims.play('avance', true);
+          bot.anims.play('walk', true);
         } else if(!this.physics.overlap(this.bot, this.player)) {
           bot.setVelocityX(-200);
         } else if (this.physics.overlap(this.bot, this.player)) {
@@ -184,33 +231,6 @@ class GameScene extends Phaser.Scene {
     /* The 'left' animation uses frames 0, 1, 2 and 3 and runs at 10 frames per second.
     The 'repeat -1' value tells the animation to loop.
     */
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers(WALK_KEY, { start: 0, end: 8 }),
-      frameRate: 20,
-      repeat: -1,
-    });
-
-    this.anims.create({
-      key: 'turn',
-      frames: this.anims.generateFrameNumbers(IDLE_KEY, { start: 0, end: 5 }),
-      frameRate: 20,
-      repeat: -1,
-    });
-
-    this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers(WALK_KEY, { start: 0, end: 8 }),
-      frameRate: 20,
-      repeat: -1,
-    });
-
-    this.anims.create({
-      key: 'attack',
-      frames: this.anims.generateFrameNumbers(ATTACK_KEY, { start: 0, end: 8}),
-      frameRate: 5,
-      repeat: 0,
-    });
 
     player.setCollideWorldBounds(true);
     player.body.onWorldBounds = true;
@@ -219,6 +239,7 @@ class GameScene extends Phaser.Scene {
   }
 
   handlePlayerBotCollision() {
+
     
     const timer = this.time.now;
     // Use an interval that lets the animation to be done only once every time and deducting the health only after that
@@ -229,13 +250,23 @@ class GameScene extends Phaser.Scene {
       if(!this.isCollisionDone){
         this.bot.setVelocityX(0);
         this.player.setVelocityX(0);
-        this.player.anims.play('attack');
-        this.bot.anims.play('attack');
+        if(currentAttack === 'attack1'){
+          this.player.anims.play('attack1');
+          currentAttack = 'attack2'
+        } else if (currentAttack === 'attack2'){
+          this.player.anims.play('attack2');
+          currentAttack = 'attack3'
+        } else {
+          this.player.anims.play('attack3')
+          currentAttack = 'attack1'
+        }
+
+        this.bot.anims.play('attack1');
         
         this.time.addEvent({
-          delay: 1,
+          delay: 280,
           callback: () => {
-            this.pertePV();
+            this.handleHealth();
             this.isCollisionDone = true;
           }
         })
@@ -244,12 +275,14 @@ class GameScene extends Phaser.Scene {
     }
   }
 
-  pertePV(){
-    this.botHealth -= 1;
-    this.playerHealth -= 1;
+  handleHealth(){
+
+
+    this.botHealth -= this.playerDamage;
+    this.playerHealth -= this.botDamage;
     this.scoreLabel.add(100);
 
-    if(this.playerHealth <1){
+    if(this.playerHealth <= 0){
       this.player.disableBody(true, true);
       this.scoreLabel.setText(`GAME OVER : ( \nYour Score = ${this.scoreLabel.score}`);
 
@@ -259,14 +292,18 @@ class GameScene extends Phaser.Scene {
       
     } else if(this.botHealth <1){
       this.bot.disableBody(true, true);
+      this.scoreLabel.add(this.previousBotHealth);
       
       setTimeout(() => {
         this.bot.enableBody(true, 600, 470, true, true);
         // increments bot health by 1 each time it respawns making it harder
         this.botHealth = this.previousBotHealth + 1;
+        this.playerHealth += 20;
+
         this.previousBotHealth = this.botHealth;
       }, 2000);
     }
+    nbRequired.setText(this.botHealth/this.playerDamage);
     info.setText(this.playerHealth);
   }
 
